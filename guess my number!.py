a8d6d2cdf2eae1108d17845cbbdf76a79ecb8502
BuildTools <unconfigured@null.spigotmc.org>
2019-08-15 12:11:01 -0700

    new commit

--- guess my number!.py
+++ guess my number!.py
@@ -1,10 +1,18 @@
-import random
+import random, sys
+end = 0
+wins = 0
 while True:
     x = random.randint(0,10)
     y = input('guess my number! It is in between 0 and 10.')
     print (y)
     print (x)
-    if x==y:
-        print('correct!')
+    if int(x)==int(y):
+        print('correct! You win!')
+        wins += 1
+        print('you have ' + str(wins) + ' wins!')
     else:
         print('wrong!')
+        end += 1
+    if end == 5:
+        print('you lose!')
+        sys.exit()
